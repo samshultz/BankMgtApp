d7877b998f4b34c48202cf2a89e820092c551894
samshultz <taiwogabrielsamuel@gmail.com>
2020-08-11 22:33:12 +0100

    updating the password change functionality

--- bankapp/views.py
+++ bankapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from allauth.account.forms import ChangePasswordForm
 
 # Create your views here.
 def profile(request):
-    return render(request, "account/profile.html", {})
+    return render(request, "account/profile.html", {"form": ChangePasswordForm})
