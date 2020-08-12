03953fc842a8483e56d32041dd65623bb58d63d4
Taiwo Samuel <taiwogabrielsamuel@gmail.com>
2020-08-12 19:22:26 +0000

    added email change functionality

--- bankapp/views.py
+++ bankapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from allauth.account.forms import ChangePasswordForm
+from allauth.account.forms import ChangePasswordForm, AddEmailForm
 from allauth.account.views import PasswordChangeView 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 
 @login_required
 def profile(request):
-    return render(request, "account/profile.html", {"form": ChangePasswordForm})
+    return render(request, "account/profile.html", {"form": ChangePasswordForm, "email_form": AddEmailForm})
 
 
 class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
